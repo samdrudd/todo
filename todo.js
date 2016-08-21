e7fb2a711956da07df0d8f7f2b464b5ee6b9ac8d
Sam <samdrudd@gmail.com>
2016-08-21 00:59:28 -0700

    Refactoring

--- todo.js
+++ todo.js
@@ -2,8 +2,19 @@ var data = JSON.parse(localStorage.getItem("todoData"));
 data = data || {};
 
 // -------------------------------------------------
-// FUNCTION: createTask
-// ACCEPTS: string title, string desc
+// PARAMS: string title, string desc
+// RETURNS: task object
+// -------------------------------------------------
+function btn_addNewTask(title, desc)
+{
+    task = createTask(title, desc);
+    addTaskToData(task);
+    addTaskToList(task);
+}
+
+
+// -------------------------------------------------
+// PARAMS: string title, string desc
 // RETURNS: task object
 // -------------------------------------------------
 function createTask(title, desc)
@@ -19,47 +30,85 @@ function createTask(title, desc)
 }
 
 // -------------------------------------------------
-// FUNCTION: addTask
-// ACCEPTS: Object task (returned from createTask)
+// PARAMS: task - task object returned from createTask
 // Adds given task to data array and localStorage
-//
 // -------------------------------------------------
-function addTask(task)
+function addTaskToData(task)
 {
   // Add task to localStorage
   data[task.id] = task;
-  var taskDiv = $("#" + task.id);
 
   localStorage.setItem("todoData", JSON.stringify(data));
-
-  addTaskToList(task);
-  taskDiv.hide();
-  taskDiv.fadeIn(500);
 }
 
 // -------------------------------------------------
-// FUNCTION: clearTasks
+// PARAMS: task - task object to add to list
+// Creates and displays HTML associated with task object
+// -------------------------------------------------
+function addTaskToList(task)
+{
+  // Create the delete button
+  var delButton = $("<span>", {
+	class: "fa fa-ban action delete",
+	title: "Delete",
+	onClick: "btn_deleteTask(" + task.id + ");"});
+
+  // Create done button
+  var doneButton = $("<span>", {
+    id: task.id + "-done",
+	class: "fa fa-check-circle-o action done",
+	title: "Done",
+	onClick: "btn_finishTask(" + task.id + ");"});
+
+  // Create task div
+  var taskDiv = $("<div>", {
+    id: task.id,
+	style: "",
+	class: "task"});
+
+  taskDiv.append(delButton);
+  taskDiv.append(doneButton);
+
+  taskDiv.append("<h3>" + task.title + "</h3><h4>Created: " + task.created + "</h4><span class='description' style=''>" + task.desc + "</span>");
+
+  // If task is stored as completed
+  if (task.status === 1)
+  {
+    $("#container_tasks_completed").append(taskDiv);
+    $("#" + taskid + "-done").removeClass("fa-check-circle-o").addClass("fa-check-circle");
+	taskDiv.addClass("completed");
+	$("#" + taskid + " .description, #" + taskid + " h4").hide();
+
+  }
+  else
+	$("#container_tasks_uncompleted").append(taskDiv);
+
+  taskDiv.hide();
+  taskDiv.fadeIn(500)
+}
+
+// -------------------------------------------------
 // Clears list as well as localStorage
 // -------------------------------------------------
-function clearTasks()
+function btn_deleteAllTasks()
 {
   // Prompt for confirmation
+  // @TODO: Make a better prompt for this
   if (confirm("Click OK to delete all tasks."))
   {
     // Clear list and localStorage
     localStorage.clear();
     data = {};
-    clearList(true, true);
+    clearAllTasks();
   }
 }
 
 // -------------------------------------------------
-// FUNCTION: deleteTask
-// ACCEPTS: id of task to delete
+// PARAMS: taskid - id of task to delete
 // Removes task with associated id from data
 // Pushes updated data to localStorage
 // -------------------------------------------------
-function deleteTask(taskid)
+function btn_deleteTask(taskid)
 {
 	var taskObj = $("#" + taskid);
 
@@ -84,12 +133,11 @@ function deleteTask(taskid)
 }
 
 // -------------------------------------------------
-// FUNCTION: doneTask
-// ACCEPTS: id of task to mark done
+// PARAMS: id of task to mark done
 // Collapses task's div and highlights it green to
 // indicate the task has been completed
 // -------------------------------------------------
-function doneTask(taskid)
+function btn_finishTask(taskid)
 {
   var task = $("#" + taskid);
 
@@ -134,58 +182,13 @@ function doneTask(taskid)
   localStorage.setItem("todoData", JSON.stringify(data));
 }
 
-// -------------------------------------------------
-// FUNCTION: addTaskToList
-// ACCEPTS: task - task object to add to list
-// Creates and displays HTML associated with task object
-// -------------------------------------------------
-function addTaskToList(task)
-{
-  // Create the delete button
-  var delButton = $("<span>", {
-	class: "fa fa-ban action delete",
-	title: "Delete",
-	onClick: "deleteTask(" + task.id + ");"});
 
-  // Create done button
-  var doneButton = $("<span>", {
-    id: task.id + "-done",
-	class: "fa fa-check-circle-o action done",
-	title: "Done",
-	onClick: "doneTask(" + task.id + ");"});
-
-  // Create task div
-  var taskDiv = $("<div>", {
-    id: task.id,
-	style: "",
-	class: "task"});
-
-  taskDiv.append(delButton);
-  taskDiv.append(doneButton);
-
-  taskDiv.append("<h3>" + task.title + "</h3><h4>Created: " + task.created + "</h4><span class='description' style=''>" + task.desc + "</span>");
-
-
-
-  // If task is stored as completed
-  if (task.status === 1)
-  {
-    $("#container_tasks_completed").append(taskDiv);
-    $("#" + taskid + "-done").removeClass("fa-check-circle-o").addClass("fa-check-circle");
-	taskDiv.addClass("completed");
-	$("#" + taskid + " .description, #" + taskid + " h4").hide();
-
-  }
-  else
-	$("#container_tasks_uncompleted").append(taskDiv);
-}
 
 // -------------------------------------------------
-// FUNCTION: clearList
-// ACCEPTS:
+// PARAMS:
 //   completed - boolean, if true clear completed tasks
 //   uncompleted - boolean, if true clear uncompleted tasks
-// Clears out HTML list of taks (DOES NOT UPDATE localStorage)
+// Clears out HTML list of tasks (DOES NOT UPDATE localStorage)
 // @TODO: Make separate 'clearCompletedTasks' and 'clearUncompletedTasks' that call into this function
 // -------------------------------------------------
 function clearList(completed, uncompleted)
@@ -201,15 +204,32 @@ function clearList(completed, uncompleted)
 	}
 }
 
+// Clears only completed tasks
+function clearCompletedTasks()
+{
+    clearList(true, false);
+}
+
+// Clears only uncompleted tasks
+function clearUncompletedTasks()
+{
+    clearList(false, true);
+}
+
+// Clears all tasks
+function clearAllTasks()
+{
+    clearList(true, true);
+}
+
 // -------------------------------------------------
-// FUNCTION: updateList
 // Clears HTML list and updates it with data currently
 // in localStorage
 // -------------------------------------------------
 function updateList()
 {
   // Clear list
-  clearList(true, true);
+  clearAllTasks();
 
   // Repopulate with tasks from data
   for (taskid in data)
@@ -219,34 +239,14 @@ function updateList()
 }
 
 
+
+
 // DOCUMENT.READY
 $( document ).ready(function() {
 var data = {};
 
 // Initalize list from localStorage
 updateList();
-
-// -----------------------------------------------
-// FUNCTION: ADD TASK
-// Adds a task to the list and to localStorage
-// @TODO: Create separate function for this and add onClick to add task button, to follow same pattern as rest of app
-// -----------------------------------------------
-$( "#add-task" ).click(function() {
-  // Get task info from form
-  var title = $("#task-title").val();
-  var desc = $("#task-desc").val();
-
-  // If no title, don't add anything
-  if (title === "") return;
-
-  // Create task, add it to list and localStorage
-  var tasktoadd = createTask(title, desc);
-  addTask(tasktoadd);
-
-  // Clear form
-  $("#task-title").val("");
-  $("#task-desc").val("");
-}); // END FUNCTION: ADD TASK
 
 // Keep add task form with you as you scroll
 $(window).scroll(function() {
